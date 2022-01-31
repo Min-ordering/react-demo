8bd93d5dfe0a3d9017a9df27d932aa40bfce9f25
Min <skyandarti@outlook.com>
2022-02-01 04:33:48 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Blockchain expert
+          Blockchain deveooper
         </h1>
       </header>
     </div>

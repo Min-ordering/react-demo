b9cfbba659319d7594a1957215548bb1defc8508
Min <skyandarti@outlook.com>
2022-04-10 20:42:14 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Let's learn React
+          React master
         </a>
       </header>
     </div>

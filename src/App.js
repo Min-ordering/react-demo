b7383544fea1bf03d2ce481ef05f577704a89fa1
Min <skyandarti@outlook.com>
2022-04-13 16:32:01 +0800

    Update: title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React master
+          React Expert
         </a>
       </header>
     </div>

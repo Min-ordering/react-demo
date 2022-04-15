ed90266853828cd7a835be470e9ca91c821b34ce
Min <skyandarti@outlook.com>
2022-04-16 04:54:55 +0800

    Update: title

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React/Redux
+          Full Stack
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Full stack developer
-        </a>
       </header>
     </div>
   );

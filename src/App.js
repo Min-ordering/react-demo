34f90ccf45c50f5954f0392d306491fda0aa7034
Min <skyandarti@outlook.com>
2022-05-22 01:22:51 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Next expert
+          Gsap expert
         </h1>
       </header>
     </div>

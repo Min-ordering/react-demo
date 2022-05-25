183cf2aade1b9e92ff086bf7bc7d9c7eaed73130
Min <skyandarti@outlook.com>
2022-05-25 16:59:35 +0800

    Update: title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Gsap expert
+          Defi expert
         </h1>
       </header>
     </div>

e6915368c66b442aa7ada040024ac8fd093f96af
Min <skyandarti@outlook.com>
2022-06-25 16:12:52 +0800

    Update: title

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <h1>
-          Ordering expert
+          Native speaker
         </h1>
       </header>
     </div>

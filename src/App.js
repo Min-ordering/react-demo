bd702589582002628983afaf3a7fce0a4cb03307
Min <skyandarti@outlook.com>
2022-07-19 01:22:00 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1>
-          Native speaker
+          React expert
         </h1>
       </header>
     </div>

714caf52455b99d21a279dd143cc4adc4b995b76
Min <skyandarti@outlook.com>
2022-11-24 00:30:05 -0600

    develop-A

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1>
-          Typescript expert
+          Typescript
         </h1>
       </header>
     </div>

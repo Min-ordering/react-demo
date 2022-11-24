6f9f1ed9089ceb7876ab1ab37434500465055096
Min <skyandarti@outlook.com>
2022-11-24 00:56:17 -0600

    Main-55

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Main</p>
+        <p>Main-2</p>
         <h1>
           Angular
         </h1>

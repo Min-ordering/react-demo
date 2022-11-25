bdd688b66ea8fa11e14b2acde0e2ee7fa9aec070
Min <skyandarti@outlook.com>
2022-11-25 04:51:30 -0600

    Feat: develop new1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <div>Develop New1</div>
         <h1>
           Angular
         </h1>

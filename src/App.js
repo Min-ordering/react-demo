942f570c090fe372670222ae136b3722e0afd86d
Sky <min@ordering.co>
2023-05-24 05:35:28 -0600

    [Update]: main page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>
-          React
-        </h1>
-        <p>React has been developed by Facebook</p>
       </header>
     </div>
   );
